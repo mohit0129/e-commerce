763fa44346d74f4dddec49e9b3b0f1a17fcdbce3
Mohit <mohit10209@gmail.com>
2025-03-12 23:50:22 +0530

    Update config.js

--- client/config.js
+++ client/config.js
@@ -1,6 +1,7 @@
 // config.js
 const CONFIG = {
-    BASE_URL: "http://localhost:5000/",
+    // BASE_URL: "http://localhost:5000",
+    BASE_URL: "https://e-commerce-y57d.onrender.com",
   };
   
   export default CONFIG;
